8b22cd2b62181df21fc4fb24f66ecbcd4ae73e70
Abdullah Amjad <shkabdullah268@gmail.com>
2025-10-23 16:44:32 -0700

    Slider workable

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,16 +1,27 @@
-import AboutSection from "@/components/about"
-import CardLikeBanner from "@/components/cardlikebanner"
-import HeroSection from "@/components/hero"
-import OurFleetSection from "@/components/ourfleet"
-import { OurServicesSection } from "@/components/services"
-
+import AboutSection from "@/components/about";
+import CardLikeBanner from "@/components/cardlikebanner";
+import HeroSection from "@/components/hero";
+import { OurServicesSection } from "@/components/services";
+import { XclusiveSlider } from "@/components/ourfleet";
 export default function Home() {
-
-  return (<><HeroSection />
-    <AboutSection />
-    <CardLikeBanner colorheading="Ride " plainheading="in Style" paragraph="Place your trust in us for VIP chauffeur hire and enjoy an unforgettable journey." />
-    <OurFleetSection />
-    <CardLikeBanner colorheading="Schedule " plainheading=" Your Chauffeur Today!" paragraph="If you want to take your travel experience to new heights, you can contact our experts to schedule your luxury ride now."/>
-    <OurServicesSection />
-    </>)
+  return (
+    <>
+      <HeroSection />
+      <AboutSection />
+      <CardLikeBanner
+        colorheading="Ride "
+        plainheading="in Style"
+        paragraph="Place your trust in us for VIP chauffeur hire and enjoy an unforgettable journey."
+      />
+      <main className="min-h-screen bg-neutral-900 flex items-center">
+        <XclusiveSlider />
+      </main>
+      <CardLikeBanner
+        colorheading="Schedule "
+        plainheading=" Your Chauffeur Today!"
+        paragraph="If you want to take your travel experience to new heights, you can contact our experts to schedule your luxury ride now."
+      />
+      <OurServicesSection />
+    </>
+  );
 }
