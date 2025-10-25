7a502c29578c15d73f57891044ec0b77625cadce
Abdullah Amjad <shkabdullah268@gmail.com>
2025-10-25 09:57:08 -0700

    feat: gallery page

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,11 +1,8 @@
 import Herobg from "@/components/herobg";
-import Faq from "@/components/faqs/faqs";
 export default function Page() {
   return (
     <>
-      <Herobg text="FAQ's" />
-
-      <Faq />
+      <Herobg text="Our Gallery" />
     </>
   );
 }
